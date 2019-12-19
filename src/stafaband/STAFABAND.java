cd45ce23df8526b725fc7c2f6a3feea22603b201
Dimas Miftahul Huda <55841844+dimasmfth@users.noreply.github.com>
2019-12-19 22:52:19 +0700

    Add files via upload

---
+++ src/stafaband/STAFABAND.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stafaband;
+
+/**
+ *
+ * @author dimas
+ */
+public class STAFABAND {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        LoginView login = new LoginView();
+        login.setVisible(true);
+    }
+    
+}
